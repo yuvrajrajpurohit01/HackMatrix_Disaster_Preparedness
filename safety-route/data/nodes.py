c8639599f7fc87dd3cf86e861225824ae550b5d7
Yuvraj Rajpurohit <yuvrajrajpurohitt@gmail.com>
2026-04-03 14:06:15 +0530

    Add files via upload
    all the updated and real time nodes

---
+++ safety-route/data/nodes.py
@@ -0,0 +1,33 @@
+# data/nodes.py
+
+nodes = {
+    # START (MP Nagar Area)
+    "A": {"name": "Home (MP Nagar)", "type": "start", "pos": (23.2320, 77.4300)},
+
+    # NORMAL AREAS
+    "B": {"name": "Board Office Square", "type": "normal", "pos": (23.2335, 77.4325)},
+    "G": {"name": "New Market", "type": "normal", "pos": (23.2345, 77.4010)},
+    "K": {"name": "Arera Colony", "type": "normal", "pos": (23.2400, 77.4200)},
+    "M": {"name": "Shahpura", "type": "normal", "pos": (23.2200, 77.4100)},
+    "N": {"name": "BHEL Area", "type": "normal", "pos": (23.2800, 77.4200)},
+    "O": {"name": "Ayodhya Bypass", "type": "normal", "pos": (23.2600, 77.4500)},
+
+    # SHELTERS
+    "C": {"name": "Relief Shelter - TT Nagar", "type": "shelter", "pos": (23.2500, 77.4000)},
+    "I": {"name": "Emergency Shelter - Kolar", "type": "shelter", "pos": (23.1800, 77.4500)},
+    "P": {"name": "School Shelter - Govindpura", "type": "shelter", "pos": (23.2600, 77.4300)},
+
+    # SUPPLIES
+    "D": {"name": "AIIMS Bhopal", "type": "supply", "pos": (23.2000, 77.4500)},
+    "H": {"name": "Water Supply - Lake View", "type": "supply", "pos": (23.2500, 77.3900)},
+    "L": {"name": "Hamidia Hospital", "type": "supply", "pos": (23.2600, 77.3900)},
+    "Q": {"name": "Food Center - Habibganj", "type": "supply", "pos": (23.2300, 77.4400)},
+
+    # DANGER ZONES
+    "E": {"name": "Flood Zone - Upper Lake Area", "type": "danger", "pos": (23.2500, 77.3800)},
+    "J": {"name": "Blocked Road - Old City", "type": "danger", "pos": (23.2600, 77.3950)},
+    "R": {"name": "Fire Zone - Industrial Area", "type": "danger", "pos": (23.2700, 77.4300)},
+
+    # DESTINATION
+    "F": {"name": "Safe Zone - Airport Area", "type": "destination", "pos": (23.2900, 77.3500)},
+}
